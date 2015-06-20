3f9bcc8261c1241c9c2345c58321f85d10eb2d12
ariel <ariel@ariel-G41D3C.(none)>
2015-06-20 12:53:33 -0300

    Almost finished. Just need add robots methods.

--- trabalho2/trabalho2.c
+++ trabalho2/trabalho2.c
@@ -1,5 +1,11 @@
 #define MAX_X 11
-#define MAX_y 11
+#define MAX_Y 11
+
+#define MIN_OBJECT_DIST 10
+
+#define COLOR S2
+#define GIRO S3
+#define SONAR S4
 
 enum direction{ NONE, UP, DOWN, LEFT, RIGHT, WALL};
 enum bool{ false, true};
@@ -23,13 +29,14 @@ void init(){
     }
 
     origin_x = (int)(MAX_X/2);
-    origin_y = (int)(MAX_Y/2);
+    origin_y = 0;
     current_x = origin_x;
     current_y = origin_y;
     current_direction = DOWN;
+    max_deep++;
 }
 
-direction AntiClockWiseDirection( direction dir ){
+direction antiClockWiseDirection( direction dir ){
     switch(dir){
         case UP:
             return LEFT;
@@ -39,6 +46,21 @@ direction AntiClockWiseDirection( direction dir ){
             return RIGHT;
         case RIGHT:
             return UP;
+        default:
+            return NONE;
+    }
+}
+
+direction clockWiseDirection( direction dir ){
+    switch(dir){
+        case UP:
+            return RIGHT;
+        case LEFT:
+            return UP;
+        case DOWN:
+            return LEFT;
+        case RIGHT:
+            return DOWN;
         default:
             return NONE;
     }
@@ -59,48 +81,166 @@ direction oppositeDirection( direction dir ){
     }
 }
 
-void turnAntiClockWise(){
-    //TODO: função de virar o robo 90 graus
-    current_direction = AntiClockWiseDirection( current_direction );
+int getForwardX(){
+    switch( current_direction ){
+        case UP:
+            return current_x;
+        case LEFT:
+            return current_x - 1;
+        case DOWN:
+            return current_x
+        case RIGHT:
+                return current_x + 1;
+        default:
+                return current_x;
+    }
 }
 
+int getForwardY(){
+    switch( current_direction ){
+        case UP:
+            return current_y - 1;
+        case LEFT:
+            return current_y;
+        case DOWN:
+            return current_y + 1; 
+        case RIGHT:
+            return current_y;
+        default:
+            return current_y;
+    }
+}
+
+void turnAntiClockWise(){
+    //TODO: função de virar o robo 90 graus sentido anti-horario
+    current_direction = antiClockWiseDirection( current_direction );
+}
+
+void turnClockWise(){
+    //TODO: função de virar o robo 90 graus sentido horario
+    current_direction = clockWiseDirection( current_direction );
+}
+
+int readSonar(){
+    int repetitions = 0;
+    int dist = 0;
+    int new_read;
+
+    for( int count = 0 ; count < 20 ; count++ ){
+        new_read = SensorValue[SONAR];
+        if( dist == new_read ){
+            repetitions++
+        }
+        else{
+            dist = new_read;
+            repetitions = 0;
+        }
+        if( repetitions == 5 ){
+            return dist;
+        }
+        delay(10);
+    }
+}
+
+bool checkSonar(){
+    if( readSonar() < MIN_OBJECT_DIST ){
+        return true;
+    }
+    return false;
+
+}
+
+bool verifyWall( int next_x, int next_y ){
+    if( (next_x < 0)  || (next_x >= MAX_X)
+            || (next_y < 0)  || (next_y >= MAX_Y)
+            || ( parents_map[next_x][next_y] == WALL ) ){
+        return true;
+    }
+    if( parents_map[next_x][next_y] == NONE ){
+        if( checkSonar() ){
+            parents_map[next_x][next_y] = WALL;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool goToSon(){
+    int next_x = getForwardX();
+    int next_y = getForwardY();
+    if( verifyWall( next_x, next_y ) ){
+        return false;
+    }
+    goForward();
+    current_x = next_x;
+    current_y = next_y;
+    parents_map[current_x][current_y] = oppositeDirection(current_direction);
+    current_deep++;
+    return true;
+}
+
+void nextDeep(){
+    max_deep++;
+}
+
+void backToParent(){
+    do{
+        turnAntiClockWise();
+    }while( current_direction != parents_map[current_x][current_y] );
+    current_x = getForwardX();
+    current_y = getForwardY();
+    goForward();
+    current_deep--;
+}
+
+void verifyOrigin(){
+    if( current_x == origin_x ){
+        if( current_y == origin_y ){
+            if( current_direction == TOP ){
+                nextDeep();
+                do{
+                    turnAntiClockWise();
+                }while( !goToSon() );
+            }
+        }
+    }
+}
+
+void turnToNextDirection(){
+    turnClockWise();
+}
 
 int main( int argc, char **argv ){
+    bool finished = false;
+
     init();
 
     if( isFinal() ){
-        finish();
+        finished = true;
     }
-    max_deep++;
 
-    while( 1 ) {
-        if( current_x == origin_x ){
-            if( current_y == origin_y ){
-                if( current_direction == TOP ){
-                    turnToFirstDirection();
-                }
-            }
-        }
+    while( !finished ) {
+
+        verifyOrigin();
+
         if( current_deep == max_deep ){
             if( isFinal() ){
-                finish();
+                finished = true;
+                continue;
             }
             backToParent();
             continue;
         }
-        else if( previous_deep < current_deep ){
-            turnToFirstDirection();
-        }
-        else{
-            turnToNextDirection();
-        }
-        while( hasWall() ){
+
+        turnToNextDirection();
+
+        while( !goToSon() ){
             turnAntiClockWise();
+            if( current_direction == parents_map[current_x][current_y] ){
+                backToParent();
+                continue;
+            }
         }
-        if( current_direction == parents_map[current_x][current_y] ){
-            backToParent();
-            continue;
-        }
-        goToSon();
     }
+
 }
